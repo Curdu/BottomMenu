b6435f583a6b64b20a57b6faead0fb0b6694bca4
Enaitz <enaitz.etxagibel@institutvidreres.cat>
2025-10-29 08:21:43 +0100

    fdfdf

---
+++ app/src/main/java/com/enaitzdam/bottommenuzero/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.enaitzdam.bottommenuzero
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.enaitzdam.bottommenuzero.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_main)
+
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupWithNavController(binding.bottomNavigationMenu, navController)
+
+    }
+}
